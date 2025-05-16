b021da4a3be267a9fce6306dd047c144ac4e2fdc
Ender Görgülü <ender.gorgulu@asurgent.se>
2025-05-16 11:39:55 +0200

    Add meta keywords for improved SEO in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="keywords" content="Azure IP Lookup, Azure Public IP, Azure Service Tags, IP Lookup, Azure IP Search, Azure, IP, Microsoft Azure, IP Address, Service Tags" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
